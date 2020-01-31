0a56580226cace67c1b91d7cef649ba25f038485
SimonLangloisCRC <simonl13.700@gmail.com>
2020-01-30 19:23:21 -0500

    Added a scrollpane to the upgrades menu, added buttons to the game screen and added the grass png to the parallax.

--- core/src/com/mygdx/game/ParallaxBackground.java
+++ core/src/com/mygdx/game/ParallaxBackground.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 
@@ -12,7 +13,9 @@ public class ParallaxBackground extends Actor {
 
     private int scroll;
     private Array<Texture> layers;
+    private Texture grassTexture;
     private final int LAYER_SPEED_DIFFERENCE = 2;
+    public int thing = 0;
 
     float x,y,width,heigth,scaleX,scaleY;
     int originX, originY,rotation,srcX,srcY;
@@ -22,6 +25,12 @@ public class ParallaxBackground extends Actor {
 
     public ParallaxBackground(Array<Texture> textures){
         layers = textures;
+
+        grassTexture = new Texture(Gdx.files.internal("imgs/grass.png"));
+        grassTexture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+        TextureRegion textureRegion = new TextureRegion(grassTexture);
+        textureRegion.setRegion(10,0,grassTexture.getWidth()*8,grassTexture.getHeight());
+
         for(int i = 0; i <textures.size;i++){
             layers.get(i).setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
         }
@@ -36,7 +45,11 @@ public class ParallaxBackground extends Actor {
     }
 
     public void setSpeed(int newSpeed){
+
         this.speed = newSpeed;
+    }
+    public int getSpeed(){
+        return speed;
     }
 
 
@@ -49,6 +62,9 @@ public class ParallaxBackground extends Actor {
             srcX = scroll + i*this.LAYER_SPEED_DIFFERENCE *scroll;
             batch.draw(layers.get(i), x, y, originX, originY, width, heigth,scaleX,scaleY,rotation,srcX,srcY,layers.get(i).getWidth(),layers.get(i).getHeight(),flipX,flipY);
         }
+        int sourceX = scroll + 4*this.LAYER_SPEED_DIFFERENCE *scroll;
+
+        batch.draw(grassTexture,x, y, originX, originY, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()/6, scaleX, scaleY, rotation, sourceX, srcY, grassTexture.getWidth(), grassTexture.getHeight(), flipX, flipY);
     }
 }
 
--- core/src/com/mygdx/game/Screens/UpgradeScreen.java
+++ core/src/com/mygdx/game/Screens/UpgradeScreen.java
@@ -4,11 +4,24 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.game.MyGdxGame;
+
+
 
 public class UpgradeScreen implements Screen {
     private Stage stage;
@@ -16,15 +29,61 @@ public class UpgradeScreen implements Screen {
     public UpgradeScreen(Game aGame) {
         game=aGame;
         stage = new Stage(new ScreenViewport());
-        Label title = new Label("Upgrades", MyGdxGame.gameSkin);
+
+
+
+        final Label title = new Label("Upgrades", MyGdxGame.gameSkin);
         title.setFontScale(2f);
         title.setY(Gdx.graphics.getHeight()*5/6);
         title.setAlignment(Align.center);
         title.setWidth(Gdx.graphics.getWidth());
         stage.addActor(title);
 
+        TextButton backButton = new TextButton("Back", MyGdxGame.gameSkin);
+        backButton.setWidth(stage.getWidth()/10);
+        backButton.setPosition(0, 0);
+        backButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new TitleScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(backButton);
+
+        float gameWidth = Gdx.graphics.getWidth();
+        float gameHeight = Gdx.graphics.getHeight();
 
 
+        SpriteBatch batcher = new SpriteBatch();
+        List<Object> list = new List<Object>(MyGdxGame.gameSkin);
+        Label[] strings = new Label[20];
+        Label temp;
+        for (int i = 0, k = 0; i < 20; i++) {
+            temp = new Label("String: " + i, MyGdxGame.gameSkin);
+            strings[k++] = temp;
+
+        }
+        list.setItems(strings);
+        ScrollPane scrollPane = new ScrollPane(list);
+        scrollPane.setBounds(900, 0, gameWidth, gameHeight);
+        scrollPane.setSmoothScrolling(false);
+        scrollPane.setPosition(gameWidth / 2 - scrollPane.getWidth() / 4,
+                gameHeight / 2 - scrollPane.getHeight() / 4);
+        scrollPane.setTransform(true);
+        scrollPane.setScale(0.5f);
+        stage.addActor(scrollPane);
+
+        scrollPane.getChildren().items[0].addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("yeh");
+            }
+        });
+        Gdx.input.setInputProcessor(stage);
 
     }
     @Override
